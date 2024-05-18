263cb0a6caf161e4f1c642cf36bb0dd3618533d7
Will Cruse <willcruse123@protonmail.com>
2024-05-18 19:34:57 +0100

    Add Transformer model

--- transformers_from_scratch/main.py
+++ transformers_from_scratch/main.py
@@ -1,12 +1,23 @@
 from pathlib import Path
+from cProfile import Profile
+from pstats import SortKey, Stats
 
 import torch
 
-from transformers_from_scratch.models import BigramLanguageModel
+from transformers_from_scratch.models import BigramLanguageModel, TransformerLanguageModel
 from transformers_from_scratch.tokenizer import CharacterTokenizer
 
-BATCH_SIZE = 32
-CONTEXT_LENGTH = 8
+BATCH_SIZE = 64
+CONTEXT_LENGTH = 256
+NUM_HEADS = 6
+NUM_LAYERS = 6
+HEAD_SIZE = 16
+DROPOUT = 0.2
+EMBEDDINGS_DIMENSION = 384
+LEARNING_RATE = 3e-4
+MAX_TRAINING_ITERATIONS = 3000
+EVAL_INTERVAL = 500
+EVAL_ITERATIONS = 200
 TORCH_SEED = 1337
 
 torch.manual_seed(TORCH_SEED)
@@ -23,12 +34,33 @@ def read_input_file(file: str) -> str:
 
 def get_batch(data: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
     data_len = len(data)
-    start_idx = torch.randint(high=data_len - CONTEXT_LENGTH, size=(BATCH_SIZE,))
+    idxs = torch.randint(high=data_len - CONTEXT_LENGTH, size=(BATCH_SIZE,))
 
-    x = torch.stack([data[i:i+CONTEXT_LENGTH] for i in start_idx])
-    y = torch.stack([data[i+1:i+CONTEXT_LENGTH+1] for i in start_idx])
+    x = torch.stack([data[i:i+CONTEXT_LENGTH] for i in idxs])
+    y = torch.stack([data[i+1:i+CONTEXT_LENGTH+1] for i in idxs])
     return (x, y,)
 
+@torch.no_grad()
+def estimate_loss(model, train_data: torch.Tensor, validation_data: torch.Tensor):
+    model.eval()
+
+    train_losses = torch.zeros(EVAL_ITERATIONS)
+    for i in range(EVAL_ITERATIONS):
+        x_batch, y_batch = get_batch(train_data)
+        _, loss = model(x_batch, y_batch)
+        train_losses[i] = loss.item()
+
+    validation_losses = torch.zeros(EVAL_ITERATIONS)
+    for i in range(EVAL_ITERATIONS):
+        x_batch, y_batch = get_batch(validation_data)
+        _, loss = model(x_batch, y_batch)
+        validation_losses[i] = loss.item()
+
+    return {
+        "train": train_losses.mean(),
+        "valid": validation_losses.mean(),
+    }
+    
 def main(input_text_file: str) -> None:
     input_text = read_input_file(input_text_file)
     
@@ -43,23 +75,39 @@ def main(input_text_file: str) -> None:
     train_data = encoded_text[:train_split_idx]
     valid_data = encoded_text[train_split_idx:]
 
-    bigram_model = BigramLanguageModel(tokenizer.vocab_size)
-    optimizer = torch.optim.Adam(params=bigram_model.parameters(), lr=1e-3)
+    # bigram_model = BigramLanguageModel(tokenizer.vocab_size)
+    transformer_model = TransformerLanguageModel(
+        tokenizer.vocab_size,
+        EMBEDDINGS_DIMENSION,
+        NUM_HEADS,
+        HEAD_SIZE,
+        CONTEXT_LENGTH,
+        NUM_LAYERS,
+        DROPOUT
+    )
+    optimizer = torch.optim.Adam(params=transformer_model.parameters(), lr=LEARNING_RATE)
+    print("Setup Model\nStarting Training...")
 
-    for step_num in range(10000):
+    for step_num in range(MAX_TRAINING_ITERATIONS):
+        if step_num % EVAL_INTERVAL == 0 or step_num == MAX_TRAINING_ITERATIONS - 1:
+            losses = estimate_loss(transformer_model, train_data, valid_data)
+            rounded_train_losses = round(losses["train"].item(), 3)
+            rounded_validation_loss = round(losses["valid"].item(), 3)
+            print(f"Step: {step_num} Training Loss: {rounded_train_losses} Validation Loss: {rounded_validation_loss}")
+
+    
         x_batch, y_batch = get_batch(train_data)
 
-        logits, loss = bigram_model(x_batch, y_batch)
-        optimizer.zero_grad()
+        logits, loss = transformer_model(x_batch, y_batch)
+        optimizer.zero_grad(set_to_none=True)
         loss.backward()
         optimizer.step()
 
-        if step_num % 100 == 0:
-            print(f"Loss: {round(loss.item(), 3)}")
-    
-    tokens = bigram_model.generate(torch.zeros((1,1), dtype=torch.long), max_new_tokens=500)[0].tolist()
+
+    tokens = transformer_model.generate(torch.zeros((1,1), dtype=torch.long), max_new_tokens=500)[0].tolist()
     decoded = tokenizer.decode(tokens)
     print(decoded)
+
     
 if __name__ == "__main__":
     main("tinyshakespeare.txt")
--- transformers_from_scratch/models.py
+++ transformers_from_scratch/models.py
@@ -1,3 +1,4 @@
+from os import fork
 import torch
 from torch import nn
 from torch.nn import functional as F
@@ -38,3 +39,204 @@ class BigramLanguageModel(nn.Module):
             idx = torch.cat((idx, idx_next), dim=1)
 
         return idx
+
+class TransformerLanguageModel(nn.Module):
+    def __init__(self,
+        vocab_size: int,
+        embedding_dim: int,
+        num_heads: int,
+        head_size: int,
+        context_length: int,
+        num_layers: int,
+        dropout: float,
+    ) -> None:
+        super().__init__()
+
+        self._vocab_size = vocab_size
+        self._embedding_dim = embedding_dim
+        self._head_size = head_size
+        self._context_length = context_length
+
+        self.token_embedding_table = nn.Embedding(
+            num_embeddings=self._vocab_size,
+            embedding_dim=self._embedding_dim
+        )
+        self.token_position_embedding_table = nn.Embedding(
+            num_embeddings=context_length,
+            embedding_dim=embedding_dim
+        )
+        
+        self.transformer_blocks = nn.Sequential(
+            *(
+                [
+                    TransformerBlock(embedding_dim, context_length, num_heads, dropout)
+                    for _ in range(num_layers)
+                ] + [nn.LayerNorm(embedding_dim)]
+            )
+        )
+                
+        self.language_model_head_linear_layer = nn.Linear(
+            in_features=embedding_dim,
+            out_features=vocab_size
+        )
+
+        
+
+    def forward(self, idx: torch.Tensor, targets=None): 
+        _, time = idx.shape
+        token_embeddings = self.token_embedding_table(idx)
+        token_position_embeddings = self.token_position_embedding_table(
+            torch.arange(time,)
+        )
+
+        x = token_embeddings + token_position_embeddings
+        x = self.transformer_blocks(x)
+        
+        logits = self.language_model_head_linear_layer(x)
+
+        if targets is None:
+            return logits, None
+            
+        batch, time, channel = logits.shape
+
+
+        batch_x_time = batch * time
+        logits_reshaped = logits.view(batch_x_time, channel)
+        targets_reshaped = targets.view(batch_x_time)
+        loss = F.cross_entropy(input=logits_reshaped, target=targets_reshaped)
+
+        return logits, loss
+
+    def generate(self, idx, max_new_tokens: int):
+        for _ in range(max_new_tokens):
+            idx_crop = idx[:, -self._context_length:]
+            logits, loss = self(idx_crop)
+
+            logits_last_timestep = logits[:,-1,:]
+            probs = F.softmax(logits_last_timestep, dim=-1)
+
+            idx_next = torch.multinomial(probs, num_samples=1)
+            idx = torch.cat((idx, idx_next), dim=1)
+
+        return idx
+
+
+class TransformerBlock(nn.Module):
+    def __init__(
+        self,
+        embedding_dim: int,
+        context_length: int,
+        num_heads: int,
+        dropout: float,
+     ) -> None:
+        super().__init__()
+
+        self.multi_head_attention_layer = MultiHeadAttention(
+            num_heads,
+            embedding_dim // num_heads,
+            embedding_dim,
+            context_length,
+            dropout,
+        )
+
+        self.feed_forward_network = FeedForwardNetwork(embedding_dim, dropout)
+        self.layer_norm_1 = nn.LayerNorm(embedding_dim)
+        self.layer_norm_2 = nn.LayerNorm(embedding_dim)
+
+    def forward(self, x):
+        x = x + self.multi_head_attention_layer(self.layer_norm_1(x))
+        x = x + self.feed_forward_network(self.layer_norm_2(x))
+        
+        return x        
+    
+class MultiHeadAttention(nn.Module):
+    def __init__(
+        self, 
+        num_heads: int,
+        head_size: int,
+        embedding_dim: int,
+        context_length: int,
+        dropout: float,
+    ) -> None:
+        super().__init__()
+
+        self.heads = nn.ModuleList([
+            AttentionHead(
+               head_size,
+               embedding_dim,
+               context_length,
+               dropout,
+           )
+           for _ in range(num_heads)
+        ])
+
+        self.projection_layer = nn.Linear(
+            in_features=embedding_dim,
+            out_features=embedding_dim,
+            bias=False,
+        )
+        self.dropout_layer = nn.Dropout(dropout)
+
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.projection_layer(out)
+        out = self.dropout_layer(out)
+        return out
+
+class AttentionHead(nn.Module):
+    def __init__(
+        self,
+        head_size: int,
+        embedding_dim: int,
+        context_length: int,
+        dropout: float,
+    ) -> None:
+        super().__init__()
+
+        self._head_size = head_size
+        self._embedding_dim = embedding_dim
+        self._context_length = context_length
+
+        self.key_layer = nn.Linear(in_features=embedding_dim, out_features=head_size, bias=False)
+        self.query_layer = nn.Linear(in_features=embedding_dim, out_features=head_size, bias=False)
+        self.value_layer = nn.Linear(in_features=embedding_dim, out_features=head_size, bias=False)
+        self.dropout_layer = nn.Dropout(dropout)
+
+        self.register_buffer(
+            "tril",
+            torch.tril(
+                torch.ones(
+                   (context_length, context_length)
+               )
+           )
+        )
+
+    def forward(self, x: torch.Tensor):
+        _, time, _ = x.shape # Batch Size, Context Length, Head Size
+
+        q = self.query_layer(x)
+        k = self.key_layer(x)
+        v = self.value_layer(x)
+    
+        weights = (q @ k.transpose(-2, -1)) * self._head_size**-0.5
+        weights = weights.masked_fill(self.tril[:time, :time] == 0, float("-inf"))
+        weights = F.softmax(input=weights, dim=-1)
+        weights = self.dropout_layer(weights)
+
+        out = weights @ v
+        return out
+
+class FeedForwardNetwork(nn.Module):
+    def __init__(self, embedding_dim: int, dropout: float) -> None:
+        super().__init__()
+
+        self.ffn = nn.Sequential(
+            nn.Linear(in_features=embedding_dim, out_features=embedding_dim * 4),
+            nn.ReLU(),
+            nn.Linear(in_features=embedding_dim * 4, out_features=embedding_dim),
+            nn.Dropout(dropout),
+        )
+
+    def forward(self, x):
+        return self.ffn(x)
+
